8deb027362ff968ace59b333cd0ff4b5f63fc44c
himanshikohli19 <himanshi.kohli19@gmail.com>
2022-01-04 13:43:07 +0530

    Sherlock and array - o(n^2)

---
+++ Sherlock_and_array-unoptimized.py
@@ -0,0 +1,66 @@
+'''
+SHERLOCK AND ARRAY - Unoptimized Solution
+https://www.hackerrank.com/challenges/sherlock-and-array/problem?isFullScreen=true
+
+Watson gives Sherlock an array of integers. His challenge is to find an element of the array
+ such that the sum of all elements to the left is equal to the sum of all elements to the right.
+
+Sample Input 0
+2
+3
+1 2 3
+4
+1 2 3 3
+
+Sample Output 0
+NO
+YES
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    l=0
+    r=len(arr)
+    suml=0
+    sumr=0
+    while(l<r):
+        sumr=sum(arr[l:r])
+        suml=sum(arr[0:l-1])
+        if suml==sumr:
+            return "YES"
+        else:
+            l+=1
+    
+    return "NO"
+        
+
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
