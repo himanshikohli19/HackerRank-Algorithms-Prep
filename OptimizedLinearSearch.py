7bd8fff2630f217a2d5ac58fd9fe69c8e4a42962
himanshikohli19 <himanshi.kohli19@gmail.com>
2022-02-06 18:40:21 +0530

    Linear Search Optimized

--- OptimizedLinearSearch.py
+++ OptimizedLinearSearch.py
@@ -1,10 +1,12 @@
 #Linear Search Optimised - from left and right side search simulaneously 
+#Time Complexity: O(log N)
+
 arr=[1,4,5,6,7,8,9,10,13,34,54,56,24,78,59,98]
 print('Array: ',arr)
 s=int(input('Enter the element to search for: '))
 l=0
 r=len(arr)
-while l<r:
+while l<(len(arr)//2)+1:
     if s==arr[l]:
         print('Element found at position: ', l+1, 'found in ', l, ' attempts' )
     if s==arr[r-1]:
